5570bce37916d49fff9e599e45f795338ec76abe
trumpet <zh19970205@126.com>
2017-02-20 18:14:22 +0800

    update

--- .build.sh
+++ .build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo build doing
-for i in *.tex; do pdflatex -synctex=1 -interaction=nonstopmode $i; done;
+for i in *.tex; do if pdflatex -synctex=1 -interaction=nonstopmode $i; then echo Success; else echo Fail; exit 1;fi; done;
 echo build done
